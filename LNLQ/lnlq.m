734920c0a5e6199fd2e05b3f9ce9cf3848b277e5
restrin <ronestrin756@gmail.com>
2019-08-22 09:22:11 -0700

    Added SYMMLQ

--- LNLQ/lnlq.m
+++ LNLQ/lnlq.m
@@ -364,7 +364,7 @@ function [x, y, flag, it, normr, resvec, errvec] = ...
           err = sqrt(err_x^2 + err_y^2);
 
           errvec(it+1) = err;
-          test4 = (err < etol * max(sqrt(normx2),1));
+          test4 = (err < etol * max(sqrt(normx2 + normy2),1));
       end
 
       if( test0 + test1 + test4 > 0 )
